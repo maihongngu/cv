a13e5566075bb5f6865fa85d13d5217c48be2ff3
Ngu Mai <vjpking5913@gmail.com>
2020-04-07 18:45:21 +0000

    Updates

---
+++ precache-manifest.285ba1c7db1762283f34c71be7f3e8b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd325a1e08bd3f19656b0f906c4039db",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "559971209402e16bcd0a",
+    "url": "/cv/static/css/2.e273ad96.chunk.css"
+  },
+  {
+    "revision": "93f47ab743e4578f7b36",
+    "url": "/cv/static/css/main.48f8ed07.chunk.css"
+  },
+  {
+    "revision": "559971209402e16bcd0a",
+    "url": "/cv/static/js/2.d6f3a774.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.d6f3a774.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15194ad78dafae70c286",
+    "url": "/cv/static/js/3.39379c7a.chunk.js"
+  },
+  {
+    "revision": "07e38a9e0acb0284376d",
+    "url": "/cv/static/js/4.7a490db7.chunk.js"
+  },
+  {
+    "revision": "e065a3280ac0d770df6a",
+    "url": "/cv/static/js/5.282303e0.chunk.js"
+  },
+  {
+    "revision": "93f47ab743e4578f7b36",
+    "url": "/cv/static/js/main.79fa89c4.chunk.js"
+  },
+  {
+    "revision": "abad2a9625321a80d78e",
+    "url": "/cv/static/js/runtime-main.b471da4f.js"
+  },
+  {
+    "revision": "5c1229f5f260968cc7603aa4ce77df10",
+    "url": "/cv/static/media/bg.5c1229f5.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.18a092883507d11ea443084c15108fd1.js"
+  "/cv/precache-manifest.285ba1c7db1762283f34c71be7f3e8b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cv/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
