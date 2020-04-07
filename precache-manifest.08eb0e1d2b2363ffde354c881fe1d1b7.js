22a1dbcfec0048bde0047c03f53b379589562c0d
Ngu Mai <vjpking5913@gmail.com>
2020-04-07 23:59:19 +0700

    Updates

---
+++ precache-manifest.08eb0e1d2b2363ffde354c881fe1d1b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c13dd6d3559c9fb4dedeb06f7f571c3",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "970a3e50a2e7c7207abd",
+    "url": "/cv/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "5c36f169516306aad5c5",
+    "url": "/cv/static/css/main.96b7af32.chunk.css"
+  },
+  {
+    "revision": "970a3e50a2e7c7207abd",
+    "url": "/cv/static/js/2.91019234.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.91019234.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a652465441401dfa982",
+    "url": "/cv/static/js/3.9d4bb673.chunk.js"
+  },
+  {
+    "revision": "92d2da9b76958643d280",
+    "url": "/cv/static/js/4.2feb2f09.chunk.js"
+  },
+  {
+    "revision": "7283725dd1931bf2d29d",
+    "url": "/cv/static/js/5.97fde72c.chunk.js"
+  },
+  {
+    "revision": "5c36f169516306aad5c5",
+    "url": "/cv/static/js/main.07c39b5f.chunk.js"
+  },
+  {
+    "revision": "bb6284cdb4ae5145e33f",
+    "url": "/cv/static/js/runtime-main.851ee727.js"
+  },
+  {
+    "revision": "5c1229f5f260968cc7603aa4ce77df10",
+    "url": "/cv/static/media/bg.5c1229f5.JPG"
+  }
+]);
