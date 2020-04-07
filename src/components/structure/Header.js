18c29b5c54c3786ca6e2e30c3aa57b97fa6551d0
Ngu Mai <vjpking5913@gmail.com>
2020-04-07 23:57:04 +0700

    Add missing push

--- src/components/structure/Header.js
+++ src/components/structure/Header.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Hero from "../elements/Hero"
-import Countdown from "../services/Countdown"
 
 
 function Header() {
     return(
         <header id="header">
             <Hero />
-            <Countdown />
         </header>
     )
 }
