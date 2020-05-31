fbd5b3df33b5963d303505f4dae42160df0ade8a
Ngu Mai <vjpking5913@gmail.com>
2020-05-31 20:22:53 +0700

    Fix bug download CV

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -2,10 +2,6 @@ import React from "react"
 import NavBar from "./Navbar"
 import Resume from "../../resume.json"
 
-const downloadCV = () => {
-    window.location.href = "/images/curriculumvitae.pdf"
-}
-
 const Hero = () => {
     return (
         <section className="hero is-fullheight has-bg-image">
@@ -17,7 +13,7 @@ const Hero = () => {
                     <p className="subtitle is-5 has-text-white">I'm a</p>
                     <h1 className="title has-text-white">{Resume.basics.label}</h1>
                     <h2 className="subtitle has-text-white">{Resume.basics.location.region}, {Resume.basics.location.country}</h2>
-                    <button className="btn btn-4 btn-4a icon-arrow-right" onClick={downloadCV}>Download</button>
+                    <a className="btn btn-4 btn-4a icon-arrow-right" href="/images/curriculumvitae.pdf">Download</a>
                 </div>
             </div>
         </section>
