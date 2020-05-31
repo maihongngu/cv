62eb1af02453e46306e33c1b56fb514f9ba5ec47
Ngu Mai <vjpking5913@gmail.com>
2020-05-31 20:10:37 +0700

    Add CV

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -2,7 +2,11 @@ import React from "react"
 import NavBar from "./Navbar"
 import Resume from "../../resume.json"
 
-function Hero() {
+const downloadCV = () => {
+    window.location.href = "/images/curriculumvitae.pdf"
+}
+
+const Hero = () => {
     return (
         <section className="hero is-fullheight has-bg-image">
             <div className="hero-head">
@@ -13,7 +17,7 @@ function Hero() {
                     <p className="subtitle is-5 has-text-white">I'm a</p>
                     <h1 className="title has-text-white">{Resume.basics.label}</h1>
                     <h2 className="subtitle has-text-white">{Resume.basics.location.region}, {Resume.basics.location.country}</h2>
-                    <button className="btn btn-4 btn-4a icon-arrow-right">Download CV</button>
+                    <button className="btn btn-4 btn-4a icon-arrow-right" onClick={downloadCV}>Download</button>
                 </div>
             </div>
         </section>
