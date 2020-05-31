f7e6acff3700450ee258e89cc7809a5c9c1ae363
Ngu Mai <vjpking5913@gmail.com>
2020-05-31 21:46:14 +0700

    Custom Button Download

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -13,7 +13,12 @@ const Hero = () => {
                     <p className="subtitle is-5 has-text-white">I'm a</p>
                     <h1 className="title has-text-white">{Resume.basics.label}</h1>
                     <h2 className="subtitle has-text-white">{Resume.basics.location.region}, {Resume.basics.location.country}</h2>
-                    <a className="btn btn-4 btn-4a icon-arrow-right" href="images/curriculumvitae.pdf">Download</a>
+                    <a href="images/curriculumvitae.pdf">
+                        <button class="btn btn-5 btn-5a icon-cart">
+                            
+                                <span>Download</span>   
+                        </button> 
+                    </a>
                 </div>
             </div>
         </section>
