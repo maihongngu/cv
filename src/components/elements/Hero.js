47a63dc8ffaccde78eaace1ece815120d05fb4f4
Ngu Mai <vjpking5913@gmail.com>
2020-05-31 20:32:52 +0700

    Fix bug download CV 3

--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
                     <p className="subtitle is-5 has-text-white">I'm a</p>
                     <h1 className="title has-text-white">{Resume.basics.label}</h1>
                     <h2 className="subtitle has-text-white">{Resume.basics.location.region}, {Resume.basics.location.country}</h2>
-                    <a className="btn btn-4 btn-4a icon-arrow-right" href="cv/images/curriculumvitae.pdf">Download</a>
+                    <a className="btn btn-4 btn-4a icon-arrow-right" href="images/curriculumvitae.pdf">Download</a>
                 </div>
             </div>
         </section>
