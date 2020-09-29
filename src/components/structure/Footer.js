c98254b7f5d1a49d9110d993d8af35df88fa947e
Ngu Mai <vjpking5913@gmail.com>
2020-09-30 01:00:34 +0700

    Edit Quote Author

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -3,12 +3,12 @@ import React from "react"
 function Footer() {
     return (
         <footer className="footer has-background-dark">
-            <div className="content has-text-centered has-text-white">
+            <div className="content has-text-centered has-text-white container">
                 <i>
                     <sup><i className="fas fa-quote-left"></i></sup>
-                        Bạn không có quyền ghét khi bạn chưa từng yêu, bởi chỉ có những người rất yêu mới biết cảm giác đau đớn khi nơi ấy thay đổi để rồi biến nổi buồn thành sự căm ghét. 
+                        Bạn không có quyền ghét khi bạn chưa từng yêu, bởi chỉ có những người đã từng rất yêu mới biết cảm giác đau đớn khi nơi ấy thay đổi để rồi biến nổi buồn thành sự căm ghét. 
                     <sup><i className="fas fa-quote-right"></i></sup>
-                    &nbsp; <sub>- &nbsp;Stephen Curry</sub>
+                    &nbsp; <sub>- &nbsp; Jimmie Fails </sub>
                 </i>
                 <p className="has-text-grey-light">
                     © {new Date().getFullYear()} Mai Hong Ngu
