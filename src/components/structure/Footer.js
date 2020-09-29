9199bd389b558dd3cb47091673d7c5b151b2a91c
Ngu Mai <vjpking5913@gmail.com>
2020-09-30 00:56:52 +0700

    Edit Quote

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className="content has-text-centered has-text-white">
                 <i>
                     <sup><i className="fas fa-quote-left"></i></sup>
-                        Success is not an accident, success is actually a choice. 
+                        Bạn không có quyền ghét khi bạn chưa từng yêu, bởi chỉ có những người rất yêu mới biết cảm giác đau đớn khi nơi ấy thay đổi để rồi biến nổi buồn thành sự căm ghét. 
                     <sup><i className="fas fa-quote-right"></i></sup>
                     &nbsp; <sub>- &nbsp;Stephen Curry</sub>
                 </i>
