54175d20bc38ab3d8428292db13d20f12880cc5a
Mai Hồng Ngự <vjpking5913@gmail.com>
2022-07-14 22:34:05 +0700

    update layout

--- src/components/sections/Box.js
+++ src/components/sections/Box.js
@@ -20,15 +20,43 @@ function Box() {
         <div>
           <div className="hero-body">
             <div className="container">
-              <p className="subtitle is-5 has-text-white">I'm a</p>
-              <h1 className="title has-text-white">{Resume.basics.label}</h1>
-              <h2 className="subtitle has-text-white">
+              <p
+                className={`subtitle is-5 has-text-white`}
+              >
+                I'm a
+              </p>
+              <h1
+                className={`title has-text-white ${css`
+                  text-shadow: -0.2rem -0.2rem 1rem red, 0.2rem 0.2rem 1rem red,
+                    0 0 2rem var(--neon-text-color),
+                    0 0 4rem var(--neon-text-color),
+                    0 0 6rem var(--neon-text-color),
+                    0 0 8rem var(--neon-text-color),
+                    0 0 10rem var(--neon-text-color);
+                `}`}
+              >
+                {Resume.basics.label}
+              </h1>
+              <h2
+                className={`subtitle has-text-white ${css`
+                  text-shadow: -0.2rem -0.2rem 1rem #ffc203,
+                    0.2rem 0.2rem 1rem #ffc203, 0 0 2rem var(--neon-text-color),
+                    0 0 4rem var(--neon-text-color),
+                    0 0 6rem var(--neon-text-color),
+                    0 0 8rem var(--neon-text-color),
+                    0 0 10rem var(--neon-text-color);
+                `}`}
+              >
                 {Resume.basics.location.region},{" "}
                 {Resume.basics.location.country}
               </h2>
-              <a href={Resume.basics.cv}>
-                <button className="btn btn-5 btn-5a icon-cart">
-                  <span>Download</span>
+              <a className="cyperpunk" href={Resume.basics.cv}>
+                <button
+                  className={`btn btn-5 btn-5a ${css`
+                    line-height: 0;
+                  `}`}
+                >
+                  Download
                 </button>
               </a>
             </div>
