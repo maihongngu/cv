fc131a6557ac9935eb52c7773ba8bb9ad163bc09
Ngu Mai <vjpking5913@gmail.com>
2022-07-22 01:28:57 +0000

    Updates

---
+++ precache-manifest.35ad8eecc7bc54eb967cdd1738d19810.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f7b06791f7ba4ce51e734fdd0046a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d5354c863a545b5d48",
+    "url": "/static/css/3.d51c4783.chunk.css"
+  },
+  {
+    "revision": "203b19b0d4d28d0550ee",
+    "url": "/static/css/4.e587d06c.chunk.css"
+  },
+  {
+    "revision": "1ae5d1fa37b3497fd869",
+    "url": "/static/css/main.28bd7346.chunk.css"
+  },
+  {
+    "revision": "0a87738a90bf90cbd280",
+    "url": "/static/js/0.382ed3ed.chunk.js"
+  },
+  {
+    "revision": "c6d5354c863a545b5d48",
+    "url": "/static/js/3.fbb761e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/3.fbb761e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "203b19b0d4d28d0550ee",
+    "url": "/static/js/4.c23d336f.chunk.js"
+  },
+  {
+    "revision": "5c666dbf7360d3de922d",
+    "url": "/static/js/5.e60cf1e2.chunk.js"
+  },
+  {
+    "revision": "6ead131508fc7ca2caf8",
+    "url": "/static/js/6.e453a0da.chunk.js"
+  },
+  {
+    "revision": "1ae5d1fa37b3497fd869",
+    "url": "/static/js/main.b9f9ee02.chunk.js"
+  },
+  {
+    "revision": "86114c14735b81e7ed88",
+    "url": "/static/js/runtime-main.19f730e3.js"
+  },
+  {
+    "revision": "53bfc124742236efdd30184c00d40745",
+    "url": "/static/media/bg.53bfc124.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9e61876fa467edba93ef8c01ea2e4ea.js"
+  "/precache-manifest.35ad8eecc7bc54eb967cdd1738d19810.js"
 );
 
 self.addEventListener('message', (event) => {
